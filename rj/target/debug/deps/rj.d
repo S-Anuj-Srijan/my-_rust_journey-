67efa9d9203a33d98ab433e20ff6d4389ffa5667
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-01-31 20:55:48 +0530

    deketed a cargo project

--- rj/target/debug/deps/rj.d
+++
@@ -1,5 +0,0 @@
-C:\Users\text2\Desktop\rust\my-_rust_journey-\rj\target\debug\deps\rj.exe: src/main.rs
-
-C:\Users\text2\Desktop\rust\my-_rust_journey-\rj\target\debug\deps\rj.d: src/main.rs
-
-src/main.rs:
