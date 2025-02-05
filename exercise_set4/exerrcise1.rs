f0887957f5e0862046bce816947334a8967e9b78
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-02-05 14:15:04 +0530

    set 4 added

---
+++ exercise_set4/exerrcise1.rs
@@ -0,0 +1,28 @@
+//exersice 1
+// Make it work with two ways
+
+/*
+fn main() {
+   let v = {
+       let mut x = 1;
+       x += 2
+   };
+
+   assert_eq!(v, 3);
+
+   println!("Success!");
+}
+*/
+// Make it work with two ways
+
+fn main() {
+    let v = {
+        let mut x = 1;
+        x += 2;
+        x
+    };
+ 
+    assert_eq!(v, 3);
+ 
+    println!("Success!");
+ }
