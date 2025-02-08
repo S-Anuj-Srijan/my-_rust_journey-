35bf5d6f6b4cde55b07bb2202f0053b379a168c3
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-02-08 18:08:16 +0530

    struct , slice , tuple added

---
+++ slice/exerrcise1.rs
@@ -0,0 +1,23 @@
+//exersice 1
+
+/*
+// Fix the error
+// Fix the errors, DON'T add new lines!
+fn main() {
+    let arr = [1, 2, 3];
+    let s1: [i32] = arr[0..2];
+
+    let s2: str = "hello, world" as str;
+
+    println!("Success!");
+}
+*/
+// Fix the errors, DON'T add new lines!
+fn main() {
+    let arr = [1, 2, 3];
+    let s1: &[i32] = &arr[0..2];
+
+    let s2: &str = "hello, world" as &str;
+
+    println!("Success!");
+}
---
+++ tuple/exercise2.rs
@@ -0,0 +1,19 @@
+//exersice 2
+
+/*
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "sunface");
+
+    println!("Success!");
+}
+*/
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+
+    println!("Success!");
+}
