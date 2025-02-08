f60a4fda294d2ac30e2f8e94e48a04be67ae735a
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-02-08 19:16:01 +0530

    enum added

---
+++ Flow control/exercise3.rs
@@ -0,0 +1,29 @@
+//exersice 3
+
+/*
+// Solve it in two ways
+// DON'T let `println!` work
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    // Implement this function, don't modify the fn signatures
+    
+}
+*/
+
+// Solve it in two ways
+// DON'T let `println!` work
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    panic!();
+    
+}
